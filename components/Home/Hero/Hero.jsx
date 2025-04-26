2014418d86ae70e9571033b8f51dbb5592a05a73
nikholas117 <nikholas123@hotmail.com>
2025-04-26 12:40:18 -0400

    fixing padding

--- components/Home/Hero/Hero.jsx
+++ components/Home/Hero/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
     <div className="w-[95%] min-h-screen relative mx-auto mt-[20vh] ">
       {/* content */}
       <div className="relative z-10 text-white flex flex-col items-center justify-center">
-        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center bg-gradient-to-r from-orange-300 to-cyan-500 bg-clip-text text-transparent">
+        <h1 className="text-2xl pb-2 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center bg-gradient-to-r from-orange-300 to-cyan-500 bg-clip-text text-transparent">
           Create Beautiful Image with <br /> Artificial Intelligence{" "}
         </h1>
         <p className="mt-3 text-sm md:text-base font-semibold text-center text-gray-300">
